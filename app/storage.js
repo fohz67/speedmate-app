5b7d695b487e1518589083d029aba429a16ac495
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-07-27 00:29:18 +0200

    remove: ALL

--- app/storage.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const deviceSave = async (key, value) => {
-    if (value !== null && key != null) {
-        await AsyncStorage.setItem(key, value.toString());
-    }
-};
-
-export default deviceSave;
