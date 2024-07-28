0c91f246396ea232389c089e5fc3bb1f6fb9b82e
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-07-28 04:48:45 +0200

    update: app

--- App.js
+++ App.js
@@ -2,17 +2,33 @@ import 'react-native-gesture-handler';
 import 'react-native-reanimated';
 import 'intl-pluralrules';
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {StatusBar, View} from "react-native";
-import useFonts from "./hooks/useFonts";
+import {useFonts} from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, {useEffect} from 'react';
+import {StatusBar} from "react-native";
 import TabNavigator from './navigation/TabNavigator';
 import './i18n';
 
-export default function App() {
-    const fontsLoaded = useFonts();
+SplashScreen.preventAutoHideAsync();
 
-    if (!fontsLoaded) {
-        return <View></View>;
+export default function App() {
+    const [loaded, error] = useFonts({
+        'Universo-Black': require('./assets/fonts/Universo-Black.ttf'),
+        'Universo-Bold': require('./assets/fonts/Universo-Bold.ttf'),
+        'Universo-Light': require('./assets/fonts/Universo-Light.ttf'),
+        'Universo-Regular': require('./assets/fonts/Universo-Regular.ttf'),
+        'Universo-Stencil': require('./assets/fonts/Universo-Stencil.ttf'),
+        'Universo-Thin': require('./assets/fonts/Universo-Thin.ttf'),
+    });
+
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded, error]);
+
+    if (!loaded && !error) {
+        return null;
     }
 
     return (
