a63ac252f1daad7a78d4a4c2e2067aad590b86aa
benjamingayaud <benjamin.gayaud@epitech.eu>
2024-07-28 05:34:31 +0200

    add: doc and auto transporter.sh

---
+++ transporter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+APP_JSON_PATH="./app.json"
+
+increment_build_number() {
+  CURRENT_BUILD_NUMBER=$(jq -r '.expo.ios.buildNumber' $APP_JSON_PATH)
+  CURRENT_BUILD_NUMBER_INT=$((10#$CURRENT_BUILD_NUMBER))
+  NEW_BUILD_NUMBER=$((CURRENT_BUILD_NUMBER_INT + 1))
+
+  jq ".expo.ios.buildNumber = \"$NEW_BUILD_NUMBER\"" $APP_JSON_PATH > tmp.$$.json && mv tmp.$$.json $APP_JSON_PATH
+  echo "Build number incremented to: $NEW_BUILD_NUMBER"
+}
+
+build_app() {
+  echo "Starting build..."
+  eas build --platform ios --profile production -y
+}
+
+submit_app() {
+  echo "Submitting build..."
+  eas submit --platform ios -y
+}
+
+increment_build_number
+build_app
+submit_app
